31ba87c80671835abce5691550a8f86f9f1b1f99
Bhavna <bhavna.rani199@gmail.com>
2025-03-24 15:13:28 +0530

    Update TestController.java

--- src/main/java/com/integrationninjas/springbootexample/controller/TestController.java
+++ src/main/java/com/integrationninjas/springbootexample/controller/TestController.java
@@ -12,8 +12,8 @@ public class TestController {
 	@GetMapping
 	public Object hello() {
 		Map<String, String> object = new HashMap<>();
-		object.put("name", "Integration Ninjas");
-		object.put("email", "integrationninjas@gmail.com");
+		object.put("name", "Bhavna");
+		object.put("email", "bhavna123@gmail.com");
 		return object;
 	}
 
